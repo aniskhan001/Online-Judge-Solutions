fce9e9d9d85bcf23c4acc9994d2cd6d8db0f0990
Anisuzzaman Khan <aniskhan001@yahoo.com>
2013-09-14 02:53:03 +0600

    Solution of Digital Fortress (11716) added

---
+++ uva/11716 - Digital Fortress.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int test, cases=0, sqrt_check, point, k;
+    string dang, new_dang;
+
+    cin >> test;
+    cin.ignore(); // stops getline from skipping the first input
+
+    while(test--){
+        getline(cin, dang);
+        sqrt_check = sqrt(dang.size());
+        if(dang.size() == sqrt_check*sqrt_check){
+            point = 0; new_dang.clear();
+            while(point < sqrt_check){
+                for(k=point; k<dang.size(); k+=sqrt_check){
+                    string dang_child(1, dang[k]);
+                    new_dang.append(dang_child);
+                }
+                point++;
+            }
+            cout << new_dang << endl;
+        }
+        else
+            cout << "INVALID\n";
+    }
+    return 0;
+}
