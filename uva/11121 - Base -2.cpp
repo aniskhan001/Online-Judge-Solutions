e8145cda9ec9aea192c95771fc27d5b8b34d57e1
Anisuzzaman Khan <aniskhan001@yahoo.com>
2013-09-26 03:58:13 +0600

    11121 - Base-2 solution added

---
+++ uva/11121 - Base -2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string rev(string dang);
+string baseMinus2(long n);
+
+int main(){
+    int test, cases=0;
+    cin >> test;
+    while(test--){
+        long n;
+        cin >> n;
+        cout << "Case #" << ++cases << ": " << baseMinus2(n) << endl;
+    }
+    return 0;
+}
+
+string baseMinus2(long n){
+    if(n == 0) return "0";
+    string res;
+    while(n){
+        if(n%(-2) == 0){
+            res.append("0");
+            n /= -2;
+        }
+        else if(n == -1){
+            res.append("11");
+            n = 0;
+        }
+        else{
+            res.append("1");
+            n = (n-1)/(-2);
+        }
+    }
+
+    return rev(res);
+}
+
+string rev(string dang){
+    int len=dang.size()-1, k;
+    string reversed;
+    for(k=len; k>=0; k--){
+        reversed.append(1, dang[k]);
+    }
+    return reversed;
+}
